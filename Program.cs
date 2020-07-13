1da768f7740b0ba0f012a43d6ab2f17b15d1be33
chayandas <1601027@kiit.ac.in>
2020-07-13 15:47:53 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchangevalues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 5;
+            x = x * y; 
+            y = x / y;
+            x = x / y;
+            Console.WriteLine("After swaping:"+ " x = " + x + ", y = " + y);
+            Console.ReadLine();
+        }
+    }
+}
